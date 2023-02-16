3a9209992ffa8ef7fb406cdae07899cee7c4fd95
Rishav Dubey <krishav49@gmail.com>
2023-02-16 13:29:59 +0530

    trying fix cros using setHeader

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ require('./config/database').connect();
 const express = require('express');
 const app=express();
 const cors = require("cors");
-app.use(cors({
-  origin:"https://curd-operation.onrender.com/"
-}));
+app.use(cors());
 
 const Item = require('./model/item');
 
@@ -18,10 +16,12 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 
 app.get('/', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.send('Hello World!');
 });
 
 app.get('/api/v1/items', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     Item.find((err, items) => {
       if (err) return res.status(500).send(err);
       res.send(items);
@@ -29,6 +29,7 @@ app.get('/api/v1/items', (req, res) => {
 });
 
 app.get('/api/v1/items/:id', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     Item.findById(req.params.id, (err, item) => {
       if (err) return res.status(500).send(err);
       if (!item) return res.status(404).send('Item not found');
@@ -38,6 +39,7 @@ app.get('/api/v1/items/:id', (req, res) => {
 
   // Create a new item
   app.post('/api/v1/items', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     const item = new Item(req.body);
     item.save((err, item) => {
       if (err) return res.status(500).send(err);
@@ -47,6 +49,7 @@ app.get('/api/v1/items/:id', (req, res) => {
   
   // Update an item by ID
   app.put('/api/v1/items/:id', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     Item.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, item) => {
       if (err) return res.status(500).send(err);
       if (!item) return res.status(404).send('Item not found');
@@ -56,6 +59,7 @@ app.get('/api/v1/items/:id', (req, res) => {
   
   // Delete an item by ID
   app.delete('/api/v1/items/:id', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     Item.findByIdAndRemove(req.params.id, (err, item) => {
       if (err) return res.status(500).send(err);
       if (!item) return res.status(404).send('Item not found');
