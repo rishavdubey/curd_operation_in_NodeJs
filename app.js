f9e938d1e745a74aab4c8782dd34b5af0b307265
Rishav Dubey <krishav49@gmail.com>
2023-02-16 12:23:21 +0530

    handle CROS err in production

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 require('./config/database').connect();
 const express = require('express');
 const app=express();
+const cors = require("cors");
+app.use(cors());
 
 const Item = require('./model/item');
 
